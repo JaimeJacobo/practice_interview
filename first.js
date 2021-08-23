a2ec4e279c09034f0661cfd497c62a8d448cefef
Jaime Jacobo <jaimejacobordv@gmail.com>
2021-08-23 12:23:58 +0200

    done

---
+++ first.js
@@ -0,0 +1,20 @@
+
+// Without using already built functions (such as reverse()), create a funtion that, given an initial array, returns a new array with all the items reversed.
+
+// Some conditions:
+
+// Items could be of any data type
+// If you receive as an input anything else rather than an array, the function should return undefined
+
+
+// Examples:
+
+//ownReverse([4, 7, 10]) --> [10, 7, 4]
+//ownReverse(['some', 'words, 'inside]) --> ['inside', 'words', 'some']
+//ownReverse(['hello']) --> ['hello']
+//ownReverse(4566) --> undefined
+
+
+const ownReverse = (items) => {
+
+};
---
+++ third.js
@@ -0,0 +1,23 @@
+
+//Without using already build in functionalities for this purpose (like toLocaleString), build your own functionality to add commas to split numbers as needed. You should put a comma every third digit from the right.
+
+// Some conditions:
+
+// It will only receive numbers greater than 0
+// If it receives any other data type rather than a number as an argument, it should return undefined
+// The output should be a string
+
+// Examples:
+
+// ownToLocaleString(10000)  --> '10,000'
+// ownToLocaleString(458370)  --> '458,370'
+// ownToLocaleString(1000000)  --> '10,000,00'
+// ownToLocaleString(345)  --> '345'
+// ownToLocaleString(45)  --> '45'
+// ownToLocaleString('hello')  --> undefined
+
+const ownToLocaleString = (number) => {
+
+};
+
+
